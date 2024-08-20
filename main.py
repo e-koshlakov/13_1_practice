2f4985d3ad9b27c00c6479e24fd3e6a98c22fd44
realman2k <53941338+realman2k@users.noreply.github.com>
2024-08-20 20:51:22 +0300

    Задание выполнено

--- main.py
+++ main.py
@@ -36,24 +36,25 @@ thread2.start()
 
 thread1.join()
 thread2.join()
-
+print()
 # Задание 3
 def write_even_numbers(numbers):
     even_numbers = [number for number in numbers if number.strip().isdigit() and int(number.strip()) % 2 == 0]
     with open("even_numbers.txt", "w") as file:
         for number in even_numbers:
             file.write(number.strip() + "\n")
-    print(f"Even numbers: {len(even_numbers)}")
+    print(f"Even numbers: {len(even_numbers)}\n")
 
 def write_odd_numbers(numbers):
     odd_numbers = [number for number in numbers if number.strip().isdigit() and int(number.strip()) % 2 != 0]
     with open("odd_numbers.txt", "w") as file:
         for number in odd_numbers:
             file.write(number.strip() + "\n")
-    print(f"Odd numbers: {len(odd_numbers)}")
+    print(f"Odd numbers: {len(odd_numbers)}\n")
 
-user_input = "numbers.txt"
-with open(user_input, "r", encoding='utf-8') as file:
+# user_input_filename = input("Enter the file name: ")
+user_input_filename = "numbers.txt"
+with open(user_input_filename, "r", encoding='utf-8') as file:
     numbers = file.read().split(',')
 
 thread1 = threading.Thread(target=write_even_numbers, args=(numbers,))
